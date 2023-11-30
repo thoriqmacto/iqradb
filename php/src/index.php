2da292f77cdf9b12979d010d861855af503ca772
thoriqmacto <thariq.hadad@gmail.com>
2023-11-30 16:24:27 +0700

    testing connect to the database

--- php/src/index.php
+++ php/src/index.php
@@ -10,11 +10,24 @@ $user = 'MYSQL_USER';
 //database user password
 $pass = 'MYSQL_PASSWORD';
 
-// check the MySQL connection status
-$conn = new mysqli($host, $user, $pass);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected to MySQL server successfully!";
+// database name
+$mydatabase = "MY_DATABASE";
+
+// check the mysql connection status
+$conn = new mysqli($host, $user, $pass, $mydatabase);
+
+// select query
+$sql = 'SELECT * FROM users';
+
+if ($result = $conn->query($sql)) {
+    while ($data = $result->fetch_object()) {
+        $users[] = $data;
+    }
+}
+
+foreach ($users as $user) {
+    echo "<br>";
+    echo $user->username . " " . $user->password;
+    echo "<br>";
 }
 ?>
